affac40ccd91e96511450d6bc3deb21d9b23883a
Schirrms <schirrms@schirrms.net>
2019-08-06 19:11:01 +0200

    Initial upload on GitHub, 'final' version (version 1.0.0)

---
+++ schirrms-subnet-ext/en.dict.schirrms-subnet-ext.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2019 Schirrms
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+	// Dictionary entries go here
+	'Class:Subnet/Attribute:dmz' => 'DMZ',
+	'Class:Subnet/Attribute:dmz/Value:internal' => 'Internal Network',
+	'Class:Subnet/Attribute:dmz/Value:back' => 'Back DMZ',
+	'Class:Subnet/Attribute:dmz/Value:front' => 'Front DMZ',
+));
+?>
---
+++ schirrms-subnet-ext/fr.dict.schirrms-subnet-ext.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2019 Schirrms
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('FR FR', 'French', 'Français', array(
+	// Dictionary entries go here
+	'Class:Subnet/Attribute:dmz' => 'DMZ',
+	'Class:Subnet/Attribute:dmz/Value:internal' => 'Réseau Interne',
+	'Class:Subnet/Attribute:dmz/Value:back' => 'DMZ Back',
+	'Class:Subnet/Attribute:dmz/Value:front' => 'DMZ Front',
+));
+?>
